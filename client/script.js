1ba17a877084191e0bc10813efe49679e3a2b1aa
victorleshe <victorleshe@gmail.com>
2022-12-27 00:30:23 +0100

    update the server url

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  const response = await fetch('https://techx.onrender.com/', {
+  const response = await fetch('https://webtechx.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
